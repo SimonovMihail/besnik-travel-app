ee7e71314eb8f1321c57221f02a26d4721c9044d
Mihail Simonov <ss-s1monov@yandex.ru>
2026-03-03 11:20:47 +0500

    Началась работа с скриптами.

---
+++ scripts/sections/slogan-block.js
@@ -0,0 +1,7 @@
+function initPeopleDropdownMenu() { // генерирует выпадающее меню
+    const peopleDropdownBlock = document.querySelector('[data-js-slogan-people-dropdown-menu]');
+    const peopleDropdownButton = peopleDropdownBlock.querySelector('[data-js-slogan-people-dropdown-menu-button]')
+
+    peopleDropdownButton.addEventListener('click', () => {
+    })
+}
