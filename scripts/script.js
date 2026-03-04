cf719dbe5b305a1d667be4492be74b6adb8a2ddf
Mihail Simonov <Tyuiu.SimonovMA@outlook.com>
2026-03-04 21:34:45 +0500

    Написал сайдбар для бургер-кнопки.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
-import { initProsSlider } from "./sections/information-block.js";
+import { initBurgerButton } from "./sections/slogan-block.js";
 import { initPeopleDropdownMenu } from './sections/slogan-block.js'
+import { initProsSlider } from "./sections/information-block.js";
 import { initAboutSlider } from "./sections/information-block.js";
 import { initPlacesSlider } from "./sections/information-block.js";
 import { initCitiesSlider } from "./sections/information-block.js";
 import { initReviewsSlider } from "./sections/information-block.js";
 
-initProsSlider();
+initBurgerButton();
 initPeopleDropdownMenu();
+initProsSlider();
 initAboutSlider();
 initPlacesSlider();
 initCitiesSlider();
--- scripts/sections/slogan-block.js
+++ scripts/sections/slogan-block.js
@@ -1,3 +1,68 @@
+export function initBurgerButton() {
+    const burgerButton = document.querySelector('[data-js-slogan-burger-button]');
+
+    if (!burgerButton) return;
+
+    let currentMenu = null;
+
+    const closeMenu = (e) => {
+        if (currentMenu && !currentMenu.contains(e.target) && !burgerButton.contains(e.target)) {
+            removeMenu();
+        }
+    }
+
+    function removeMenu() {
+        currentMenu.remove();
+        currentMenu = null;
+        document.removeEventListener('click', closeMenu);
+    }
+
+    burgerButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+
+        if (currentMenu) {
+            removeMenu();
+            return;
+        }
+
+        const sideMenuWrap = document.createElement('div');
+        sideMenuWrap.classList.add('slogan-side-menu-wrap');
+        sideMenuWrap.innerHTML = `
+            <aside class="slogan-side-menu">
+                <div class="slogan-side-menu__close-and-create-container">
+                    <button type="button" class="slogan-side-menu-close-button" data-js-slogan-close-button>close</button>
+                    <button type="button" class="slogan-side-menu-create-button">Create account</button>
+                </div>
+                <ul class="slogan-side-menu__options-list">
+                    <li class="slogan-side-menu__options-item">
+                        <button type="button" class="slogan-side-menu__options-item-button">find a trip</button>
+                    </li>
+                    <li class="slogan-side-menu__options-item">
+                        <button type="button" class="slogan-side-menu__options-item-button">destinations</button>
+                    </li>
+                    <li class="slogan-side-menu__options-item">
+                        <button type="button" class="slogan-side-menu__options-item-button">why besnik</button>
+                    </li>
+                    <li class="slogan-side-menu__options-item">
+                        <button type="button" class="slogan-side-menu__options-item-button">contact</button>
+                    </li>
+                </ul>
+            </aside>
+        `;
+
+        // burgerButton.appendChild(sideMenuWrap);
+        document.body.appendChild(sideMenuWrap)
+        currentMenu = sideMenuWrap;
+
+        const closeBtn = sideMenuWrap.querySelector('[data-js-slogan-close-button]');
+        closeBtn.addEventListener('click', () => {
+            removeMenu();
+        });
+
+        document.addEventListener('click', closeMenu);
+    })
+}
+
 export function initPeopleDropdownMenu() {
     const peopleDropdownBlock = document.querySelector('[data-js-slogan-people-dropdown-menu]');
 
